ded5c0ce72dff1d98073a0d7d0d4532376051c94
Agush12 <119295521+Agush12@users.noreply.github.com>
2023-10-03 22:51:01 +0530

    Add files via upload
    here is my day 17,today i wrote factorial code with recursion,my code first checks input if its non negative or not,if its not negative then calls recursion function to calculated factorial value.

---
+++ fact_rec.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class fact_rec {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.println("Please enter a non-negative integer.");
+        } else {
+            int result = factorial(num);
+            System.out.println("Factorial of " + num + " is: " + result);
+        }
+
+        scanner.close();
+    }
+}
