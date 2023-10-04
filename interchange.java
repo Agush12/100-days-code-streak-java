17ef3cace8b64b81b45837f458fc6fe3a5e10d2e
Agush12 <119295521+Agush12@users.noreply.github.com>
2023-10-04 22:19:57 +0530

    Add files via upload
    day 18 of 100 days of code streak,Program is about swaping 2 numbers without using third variable,i have done it using XOR(exclusive or) operation 3 times b/w 2 variable.

---
+++ interchange.java
@@ -0,0 +1,11 @@
+public class interchange {
+
+    public static void main(String args[]){
+        int a=5,b=10;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(" a is "+a+" b is "+b);
+    }
+    
+}
