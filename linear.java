e7df48e391064d2198fb0bc0447bbbdec875a933
Agush12 <119295521+Agush12@users.noreply.github.com>
2023-10-05 22:08:06 +0530

    Add files via upload
    day 19 of code streak,today i programmed about linear search algoritm,linearsearch function takes array and target value,if it is present it returns the index at which value is found else it returns -1.

---
+++ linear.java
@@ -0,0 +1,23 @@
+public class linear {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int targetValue = 7;
+
+        int result = linearSearch(array, targetValue);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
