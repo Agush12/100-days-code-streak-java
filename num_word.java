ec0897d3f06667d83bf89c1a1f24188e0a66a8c7
Agush12 <119295521+Agush12@users.noreply.github.com>
2023-11-25 22:20:18 +0530

    Add files via upload
    here is day 22,here i programmed about telling digits of an integer in word format like 1002 to one zero zero two,i used concept of string concatination and switch case here.

---
+++ num_word.java
@@ -0,0 +1,29 @@
+public class abc {
+    public static void main(String args[]){
+        String str ="";
+        int a=1024;
+        str =str +a;
+        System.out.println(str);
+        for(int i=0;i<=str.length()-1;i++){
+            char c=str.charAt(i);
+        switch(c){
+            case '0':System.out.println("zero");
+            break;
+            case '1':System.out.println("one");
+            break;
+            case '2':System.out.println("two");
+            break;
+            case '3':System.out.println("three");
+            break;
+            case '4':System.out.println("four");
+            break;
+
+        }
+
+
+    }
+
+
+    }
+    
+}
