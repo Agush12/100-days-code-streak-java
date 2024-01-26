ac547c3e9594b44d771bd05d951449ce3ea4a5c0
Agush12 <119295521+Agush12@users.noreply.github.com>
2024-01-26 22:09:14 +0530

    Add files via upload
    day 47,program is to remove an integer named val from array and move them to end and return number of elements!= val.

--- remove.java
+++ remove.java
@@ -1,24 +1,22 @@
-import java.util.*;
-public class remove{
-    static int [] remDup(int arr[]){
-        int temp[]=new int[arr.length];
-        temp[0]=arr[0];
-       int res=1;
-       for(int i=1;i<arr.length;i++){
-        if(temp[res-1]!=arr[i])
-        {temp[res]=arr[i];
-        res++;}
-       }
-       return temp;
-
+class Solution {
+    public static int []swap(int arr[],int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        return arr;
     }
-
-public static void main(String args[])
-{Scanner sc = new Scanner(System.in);
-    int arr[] ={10,10,20,20,30,40,40,40,40};
-    arr=remDup(arr);
-    for(int x:arr){
-        System.out.println(x);
+    public static int removeElement(int[] nums, int val) {
+        int count =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+               nums= swap(nums,i,count);
+                count++;
+            }
+        }return count;
+        
     }
-
-}}
+    public static void main(String args[]){
+        int arr[]={1,2,2,4,2,5,6};
+        System.out.println(removeElement(arr,2));
+    }
+}
