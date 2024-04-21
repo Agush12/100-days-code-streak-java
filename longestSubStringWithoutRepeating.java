4a95514c7fbe0d4eae04f24b7a407517a94f700f
Agush12 <119295521+Agush12@users.noreply.github.com>
2024-04-21 21:50:50 +0530

    Add files via upload
    day 77,program is to find longest substring without repeating character.

---
+++ longestSubStringWithoutRepeating.java
@@ -0,0 +1,19 @@
+class longestSubStringWithoutRepeating {
+    public static int lengthOfLongestSubstring(String s) {
+        boolean[] ss = new boolean[128];
+        int ans = 0;
+        for (int i = 0, j = 0; j < s.length(); ++j) {
+            char c = s.charAt(j);
+            while (ss[c]) {
+                ss[s.charAt(i++)] = false;
+            }
+            ss[c] = true;
+            ans = Math.max(ans, j - i + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String args[]){
+        System.out.println(lengthOfLongestSubstring("abbbabcde"));
+    }
+}
